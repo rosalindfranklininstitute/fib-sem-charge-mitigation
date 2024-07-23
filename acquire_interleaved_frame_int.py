8b740116993b1937e7e0f882da3f76208e009b5c
AbnerVelazco <92376831+AbnerVelazco@users.noreply.github.com>
2024-07-23 21:43:01 +0100

    Update acquire_interleaved_frame_int.py

--- acquire_interleaved_frame_int.py
+++ acquire_interleaved_frame_int.py
@@ -1,5 +1,5 @@
 '''
-Copyright <year> Rosalind Franklin Institute
+Copyright <2024> Rosalind Franklin Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
